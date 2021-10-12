33f473950f6caf5a855ada4c9c368ba3835214e1
yvesemmanuel <yefo@cin.ufpe.br>
2021-10-12 19:31:21 -0300

    fibonacci with recursion

--- solutions/Q10.asm
+++ solutions/Q10.asm
@@ -1,5 +1,5 @@
 .text
-	li $a0, 4 # N = 20
+	li $a0, 20 # $a0 is the argument to the function, i.e, N Fibonacci term
 	
 	Fibonacci:
 		bgt $a0, 1, Recursion # if N > 1 -> Recursion
@@ -7,7 +7,7 @@
 		jr $ra # jump to the return address, i.e, copies the content of $ra to the PC
 	
 	Recursion:
-		addi $sp, $sp, -12 # storing 3 registers (slots of memory) to the stack
+		addi $sp, $sp, -12 # allocating 3 registers (slots of memory) to the stack
 		sw $ra, 0($sp) # save $ra as the first register
 		sw $a0, 4($sp) # save $a0 as the second register (storing N)
 		
@@ -19,7 +19,7 @@
 		sub $a0, $a0, 2 # compute N - 2
 		jal Fibonacci # call the function to N - 2, but save the return address to $ra
 		
-		lw $t0, 8($sp) # store Fibonacci result, i.e, $t0 = F[N]
+		lw $t0, 8($sp) # store Fibonacci result, i.e, $t0 = F[N ]
 		add $v0, $t0, $v0 # return result, i.e, $v0 = $t0
 		
 		lw $ra, 0($sp) # restore $ra
